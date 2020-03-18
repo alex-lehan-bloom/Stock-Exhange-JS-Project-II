9ea1169721163ac7c305dfc49bbda89df3e729f5
Alex Bloom <lehanbloom@gmail.com>
2020-03-18 13:50:51 +0200

    Completed 2.1: auto-search functionality using debounce function.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+
+let searchBar = document.getElementById("searchBar");
+searchBar.onkeyup = debounce(() => {
+  let userInput = document.getElementById("searchBar").value;
+  if (userInput.length === 0) {
+    let ul = document.getElementById("searchResults");
+    ul.innerHTML = "";
+  } else {
+    search(userInput, data => {
+      displaySearchResults(data);
+    });
+  }
+}, 400);
+
 let searchButton = document.getElementById("searchButton");
 searchButton.addEventListener("click", () => {
   let userInput = document.getElementById("searchBar").value;
@@ -7,6 +21,7 @@ searchButton.addEventListener("click", () => {
 });
 
 async function search(userInput, callback) {
+  console.log("SEARCHING");
   showSpinner();
   let response = await fetch(
     `https://financialmodelingprep.com/api/v3/search?query=${userInput}&limit=10&exchange=NASDAQ`
@@ -16,7 +31,6 @@ async function search(userInput, callback) {
 }
 
 function displaySearchResults(data) {
-  console.log("hello");
   let ul = document.getElementById("searchResults");
   ul.innerHTML = "";
   for (i = 0; i < data.length; i++) {
@@ -47,4 +61,25 @@ function hideSpinner() {
   let spinner = document.getElementById("spinner");
   spinner.classList.remove("display-element");
   spinner.classList.add("hide-element");
+}
+
+function debounce(cb, interval, immediate) {
+  var timeout;
+  console.log("1");
+  return function() {
+    console.log("2");
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) cb.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, interval);
+
+    if (callNow) cb.apply(context, args);
+  };
 }
