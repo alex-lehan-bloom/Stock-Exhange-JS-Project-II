c9d5ddf2a02b9357af38c1298f1f0a937b3e06eb
Alex Bloom <lehanbloom@gmail.com>
2020-03-23 14:00:45 +0200

    Completed MS 5, I think. I made marquee functionality into a class.

--- js/marquee.js
+++ js/marquee.js
@@ -1,27 +1,35 @@
-createMarque();
-async function createMarque() {
-  let response = await fetch(
-    `https://financialmodelingprep.com/api/v3/stock/real-time-price`
-  );
-  let data = await response.json();
-  let stockInfoForMarquee = [];
-  for (let i = 0; i < 100; i++) {
-    stockInfoForMarquee.push(data.stockList[i].symbol);
-    stockInfoForMarquee.push(`$${data.stockList[i].price.toString()}`);
+class Marquee {
+  constructor(marquee) {
+    this.marquee = marquee;
   }
-  let marquee = document.getElementById("marquee");
-  let symboleIndex = 0;
-  let priceIndex = 1;
-  for (let i = 0; i < stockInfoForMarquee.length / 2; i++) {
-    let symbol = document.createElement("span");
-    symbol.textContent = stockInfoForMarquee[symboleIndex];
-    let price = document.createElement("span");
-    price.classList.add("stock-up");
-    price.textContent = stockInfoForMarquee[priceIndex];
-    let divWithStockAndPrice = document.createElement("div");
-    divWithStockAndPrice.append(symbol, price);
-    marquee.append(divWithStockAndPrice);
-    symboleIndex += 2;
-    priceIndex += 2;
+
+  async createMarque() {
+    let response = await fetch(
+      `https://financialmodelingprep.com/api/v3/stock/real-time-price`
+    );
+    let data = await response.json();
+    let stockInfoForMarquee = [];
+    for (let i = 0; i < 100; i++) {
+      stockInfoForMarquee.push(data.stockList[i].symbol);
+      stockInfoForMarquee.push(`$${data.stockList[i].price.toString()}`);
+    }
+    // let marquee = document.getElementById("marquee");
+    let symboleIndex = 0;
+    let priceIndex = 1;
+    for (let i = 0; i < stockInfoForMarquee.length / 2; i++) {
+      let symbol = document.createElement("span");
+      symbol.textContent = stockInfoForMarquee[symboleIndex];
+      let price = document.createElement("span");
+      price.classList.add("stock-up");
+      price.textContent = stockInfoForMarquee[priceIndex];
+      let divWithStockAndPrice = document.createElement("div");
+      divWithStockAndPrice.append(symbol, price);
+      marquee.append(divWithStockAndPrice);
+      symboleIndex += 2;
+      priceIndex += 2;
+    }
   }
 }
+
+let mainPageMarquee = new Marquee(document.getElementById("marquee"));
+mainPageMarquee.createMarque();
