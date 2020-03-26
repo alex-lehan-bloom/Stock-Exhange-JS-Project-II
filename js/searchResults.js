5f8d6d1025695ff7cc1f2314b440f95d084a5f2b
Alex Bloom <lehanbloom@gmail.com>
2020-03-26 21:38:30 +0200

    Completed Milestone 10.

--- js/searchResults.js
+++ js/searchResults.js
@@ -2,10 +2,13 @@ class SearchResults {
   constructor(searchResults, spinner) {
     this.searchResults = searchResults;
     this.spinner = spinner;
+    this.companiesToCompare = [];
+    this.openCompanyComparisonPage = document.getElementById(
+      "openCompanyComparisonPage"
+    );
   }
 
   displaySearchResults(listOfCompanyProfiles, userInput) {
-    console.log(listOfCompanyProfiles);
     this.userInput = userInput;
     listOfCompanyProfiles.map(profile => {
       let img = document.createElement("img");
@@ -39,7 +42,57 @@ class SearchResults {
       compareButton.classList.add("btn");
       compareButton.textContent = "Compare";
       compareButton.addEventListener("click", () => {
-        console.log(profile);
+        let companiesToCompare = document.getElementById("companiesToCompare");
+        if (companiesToCompare.querySelectorAll(".btn").length < 3) {
+          let compareCompanyName = document.createElement("span");
+          compareCompanyName.textContent = profile.symbol;
+          let cancelIcon = document.createElement("span");
+          cancelIcon.classList.add("fa", "fa-close");
+          let companyToCompare = document.createElement("button");
+          companyToCompare.classList.add("btn");
+          companyToCompare.addEventListener("click", () => {
+            this.openCompanyComparisonPage.textContent = "";
+            companyToCompare.remove();
+            for (let i = 0; i < this.companiesToCompare.length; i++) {
+              if (profile.symbol === this.companiesToCompare[i]) {
+                this.companiesToCompare.splice(i);
+              }
+              if (this.companiesToCompare.length === 1) {
+                this.openCompanyComparisonPage.textContent = `Compare ${this.companiesToCompare.length} company`;
+              } else {
+                this.openCompanyComparisonPage.textContent = `Compare ${this.companiesToCompare.length} companies`;
+              }
+            }
+            this.openCompanyComparisonPage.href = `./company.html?symbol=`;
+            console.log("length");
+            console.log(this.companiesToCompare.length);
+            for (let i = 0; i < this.companiesToCompare.length; i++) {
+              if (this.companiesToCompare.length === 1) {
+                this.openCompanyComparisonPage.href += `${this.companiesToCompare[i]}`;
+              } else {
+                this.openCompanyComparisonPage.href += `,${this.companiesToCompare[i]}`;
+              }
+            }
+          });
+          companyToCompare.append(compareCompanyName, cancelIcon);
+          companiesToCompare.append(companyToCompare);
+          this.companiesToCompare.push(profile.symbol);
+        }
+        console.log(this.companiesToCompare);
+        this.openCompanyComparisonPage.textContent = "";
+        this.openCompanyComparisonPage.href = `./company.html?symbol=`;
+        for (let i = 0; i < this.companiesToCompare.length; i++) {
+          if (i === 0) {
+            this.openCompanyComparisonPage.href += `${this.companiesToCompare[i]}`;
+          } else {
+            this.openCompanyComparisonPage.href += `,${this.companiesToCompare[i]}`;
+          }
+        }
+        if (this.companiesToCompare.length === 1) {
+          this.openCompanyComparisonPage.textContent = `Compare ${this.companiesToCompare.length} company`;
+        } else {
+          this.openCompanyComparisonPage.textContent = `Compare ${this.companiesToCompare.length} companies`;
+        }
       });
       let mainListContent = document.createElement("div");
       mainListContent.classList.add("main-list-content");
@@ -50,10 +103,6 @@ class SearchResults {
       this.searchResults.append(li);
     });
     this.hideSpinner();
-  }
-
-  compare() {
-    compareButton.addEventListener("click", () => {});
   }
 
   hideSpinner() {
