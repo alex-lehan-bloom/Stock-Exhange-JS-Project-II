4ae3fb8ca135c57a19b630bceb7610b95520d049
Alex Bloom <lehanbloom@gmail.com>
2020-05-13 14:22:10 +0300

    Clean up of search form.

--- js/searchForm.js
+++ js/searchForm.js
@@ -4,42 +4,33 @@ class SearchForm {
     this.searchResults = searchResults;
     this.spinner = spinner;
     this.spinner.classList.add("spinner-border");
-    this.searchBar = document.createElement("input");
-    this.alert = document.createElement("div");
     this.urlParams = new URLSearchParams(window.location.search);
   }
 
   createSearchBar() {
+    this.searchBar = document.createElement("input");
     this.searchBar.classList.add("form-control");
     this.searchBar.placeholder = "search";
     this.searchContainer.append(this.searchBar);
-    this.alert.classList.add("alert", "alert-danger", "hide-element");
-    this.alert.textContent = "Your search returned 0 results";
-    this.searchContainer.append(this.alert);
-  }
-
-  searchIfSymbolInURL(callback) {
-    this.symbol = this.urlParams.get("symbol");
-    if (this.symbol !== null) {
-      this.search(this.symbol, data => {
-        this.getCompanyProfile(data, listOfCompanyProfiles => {
-          callback(listOfCompanyProfiles, this.searchBar.value);
-        });
-      });
-      this.searchBar.value = this.symbol;
-    }
+    this.zeroResultsAlert = document.createElement("div");
+    this.zeroResultsAlert.classList.add(
+      "alert",
+      "alert-danger",
+      "hide-element"
+    );
+    this.zeroResultsAlert.textContent = "Your search returned 0 results";
+    this.searchContainer.append(this.zeroResultsAlert);
   }
 
   onSearch(callback) {
     this.searchBar.onkeyup = this.debounce(() => {
       this.searchQuery = this.searchBar.value;
       if (this.searchQuery.length === 0) {
-        this.searchResults.textContent = "";
-        let url = window.location.href.split("?")[0];
-        window.history.pushState({ path: url }, "", url);
+        this.clearPreviousSearchResults();
+        this.deleteURLParameters();
       } else {
-        this.search(this.searchQuery, data => {
-          this.getCompanyProfile(data, listOfCompanyProfiles => {
+        this.search((searchResults) => {
+          this.getCompanyProfiles(searchResults, (listOfCompanyProfiles) => {
             callback(listOfCompanyProfiles, this.searchBar.value);
           });
         });
@@ -47,55 +38,53 @@ class SearchForm {
     }, 400);
   }
 
-  async search(searchQuery, callback) {
+  async search(callback) {
     this.hideSearchAlert();
     this.showSpinner();
-    this.searchResults.textContent = "";
+    this.clearPreviousSearchResults();
     let response = await fetch(
-      `https://financialmodelingprep.com/api/v3/search?query=${searchQuery}&limit=10&exchange=NASDAQ`
+      `https://financialmodelingprep.com/api/v3/search?query=${this.searchQuery}&limit=10&exchange=NASDAQ`
     );
-    let data = await response.json();
-    if (data.length === 0) {
+    let searchResults = await response.json();
+    if (searchResults.length === 0) {
       this.showSearchAlert();
       this.hideSpinner();
     } else {
-      let listOfCompanySymbols = data.map(searchResponse => {
-        return searchResponse.symbol;
+      let listOfCompanySymbols = searchResults.map((company) => {
+        return company.symbol;
       });
       callback(listOfCompanySymbols);
     }
-    this.urlParams.set("symbol", searchQuery);
-    let url = window.location.href.split("?")[0] + "?" + this.urlParams;
-    window.history.pushState({ path: url }, "", url);
+    this.addSearchQueryAsURLParameter();
   }
 
-  getCompanyProfile(listOfCompanySymbols, callback) {
+  getCompanyProfiles(listOfCompanySymbols, callback) {
     let arrayOfFetchRequests = [];
-    let symbolsForFetchRequest = [];
+    let companiesForFetchRequest = [];
     let finalIndexPosition = 2;
     for (let i = 0; i < listOfCompanySymbols.length; i++) {
       if (i <= finalIndexPosition) {
-        symbolsForFetchRequest.push(listOfCompanySymbols[i]);
+        companiesForFetchRequest.push(listOfCompanySymbols[i]);
       } else {
-        symbolsForFetchRequest = [listOfCompanySymbols[i]];
+        companiesForFetchRequest = [listOfCompanySymbols[i]];
         finalIndexPosition += 3;
       }
       if (
-        symbolsForFetchRequest.length === 3 ||
+        companiesForFetchRequest.length === 3 ||
         i === listOfCompanySymbols.length - 1
       ) {
         arrayOfFetchRequests.push(
-          `https://financialmodelingprep.com/api/v3/company/profile/${symbolsForFetchRequest[0]},
-          ${symbolsForFetchRequest[1]},
-          ${symbolsForFetchRequest[2]}`
+          `https://financialmodelingprep.com/api/v3/company/profile/${companiesForFetchRequest[0]},
+          ${companiesForFetchRequest[1]},
+          ${companiesForFetchRequest[2]}`
         );
       }
     }
     Promise.all(
-      arrayOfFetchRequests.map(url =>
-        fetch(url).then(response => response.json())
+      arrayOfFetchRequests.map((url) =>
+        fetch(url).then((response) => response.json())
       )
-    ).then(data => {
+    ).then((data) => {
       for (let i = 0; i < data.length; i++) {
         callback(data[i].companyProfiles);
       }
@@ -113,29 +102,55 @@ class SearchForm {
   }
 
   showSearchAlert() {
-    this.alert.classList.remove("hide-element");
-    this.alert.classList.add("display-element");
+    this.zeroResultsAlert.classList.remove("hide-element");
+    this.zeroResultsAlert.classList.add("display-element");
   }
 
   hideSearchAlert() {
-    this.alert.classList.remove("display-element");
-    this.alert.classList.add("hide-element");
+    this.zeroResultsAlert.classList.remove("display-element");
+    this.zeroResultsAlert.classList.add("hide-element");
   }
 
-  debounce(cb, interval, immediate) { // understand me
+  clearPreviousSearchResults() {
+    this.searchResults.textContent = "";
+  }
+
+  addSearchQueryAsURLParameter() {
+    this.urlParams.set("symbol", this.searchQuery);
+    let url = window.location.href.split("?")[0] + "?" + this.urlParams;
+    window.history.pushState({ path: url }, "", url);
+  }
+
+  deleteURLParameters() {
+    let urlPath = window.location.href.split("?")[0];
+    window.history.pushState({ path: urlPath }, "", urlPath);
+  }
+
+  debounce(cb, interval, immediate) {
     let timeout;
-    return function() {
+    return function () {
       let context = this,
         args = arguments;
-      let later = function() {
+      let later = function () {
         timeout = null;
         if (!immediate) cb.apply(context, args);
       };
       let callNow = immediate && !timeout;
       clearTimeout(timeout);
       timeout = setTimeout(later, interval);
-
       if (callNow) cb.apply(context, args);
     };
+  }
+
+  searchIfSymbolInURL(callback) {
+    this.symbol = this.urlParams.get("symbol");
+    if (this.symbol !== null) {
+      this.search(this.symbol, (data) => {
+        this.getCompanyProfiles(data, (listOfCompanyProfiles) => {
+          callback(listOfCompanyProfiles, this.searchBar.value);
+        });
+      });
+      this.searchBar.value = this.symbol;
+    }
   }
 }
